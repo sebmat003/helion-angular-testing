bbdb9a4a6ae1e1ff255329eacb86382f50651a39
sebmat003 <sebmat003@utp.edu.pl>
2024-08-03 09:41:09 +0200

    added abstract class + tests

--- angular-testing-completed/src/app/examples/10-abstract-class/abstract-class-usage.component.ts
+++ angular-testing-completed/src/app/examples/10-abstract-class/abstract-class-usage.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { InputNumberComponent } from './input-number.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-abstact-class-usage',
   standalone: true,
-  template: ``,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    InputNumberComponent,
+    MatButtonModule,
+  ],
+  template: `<form [formGroup]="form" (ngSubmit)="submit()">
+    <app-input-number formControlName="control"></app-input-number>
+    <button mat-button type="submit" [disabled]="form.invalid">Submit</button>
+  </form> `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AbstractClassUsageComponent {}
+export class AbstractClassUsageComponent {
+  form = new FormGroup({
+    control: new FormControl(0),
+  });
+
+  submit() {
+    console.log(this.form.controls.control.getRawValue());
+  }
+}
