39b7af6b60e6abcb8da5c482df3032ba28372fe4
sebmat003 <sebmat003@utp.edu.pl>
2024-08-04 17:40:38 +0200

    added tests for 5 and 6

--- angular-testing-completed/cypress/e2e/examples/5-auth-guard.cy.ts
+++ angular-testing-completed/cypress/e2e/examples/5-auth-guard.cy.ts
@@ -1,15 +1,39 @@
-import { navigateTo } from "../../utils/navigate";
+import { navigateTo } from '../../utils/navigate';
 
 describe('Auth guard', () => {
   beforeEach(() => {
     navigateTo(5);
   });
 
-  it('should show admin page', () => {});
+  it('should show admin page', () => {
+    cy.get('button').contains('Login as Admin').click();
 
-  it('should show user page', () => {});
+    cy.get('button').contains('Go to the "Admin" page').click();
 
-  it('should be able to login to user/admin page when owner is logged in', () => {});
+    cy.url().should('contain', '/admin');
+  });
 
-  it('should not access to pages for not logged in user', () => {});
+  it('should show user page', () => {
+    cy.get('button').contains('Login as User').click();
+
+    cy.get('button').contains('Go to the "User" page').click();
+
+    cy.url().should('contain', '/user');
+  });
+
+  it('should be able to login to user/admin page when owner is logged in', () => {
+    cy.get('button').contains('Login as Owner').click();
+
+    cy.get('button').contains('Go to the "Admin" page').click();
+    cy.url().should('contain', '/admin');
+    cy.get('button').contains('Go to the "User" page').click();
+    cy.url().should('contain', '/user');
+  });
+
+  it('should not access to pages for not logged-in user', () => {
+    cy.get('button').contains('Go to the "Admin" page').click();
+
+    cy.url().should('not.contain', '/admin');
+    cy.url().should('eq', 'http://localhost:4200/6');
+  });
 });
--- angular-testing-completed/cypress/e2e/examples/6-ngrx-store.cy.ts
+++ angular-testing-completed/cypress/e2e/examples/6-ngrx-store.cy.ts
@@ -1,13 +1,61 @@
-import { navigateTo } from "../../utils/navigate";
+import { navigateTo } from '../../utils/navigate';
 
 describe('NgRx store', () => {
   beforeEach(() => {
     navigateTo(7);
   });
 
-  it('should add a new product to the store', () => {});
+  it('should add a new product to the store', () => {
+    const productName = 'My product';
+    const productPrice = '100';
+    cy.get('[data-cy="product-add-button"]').click();
+    cy.get('[data-cy="product-name-input"]').type(productName);
+    cy.get('[data-cy="product-price-input"]').type('100');
+    cy.get('[data-cy="product-thumbnail-select"]').click();
+    cy.get('[data-cy="product-thumbnail-select-option"]').eq(2).click();
+    cy.get('[data-cy="add-edit-save-button"]').click();
 
-  it('should remove a product from the store', () => {});
+    cy.get('.product').should('have.length', 4);
+    cy.get('.product')
+      .eq(4)
+      .find('.product-name')
+      .should('contain.text', productName);
+    cy.get('.product')
+      .eq(4)
+      .find('.product-price')
+      .should('contain.text', productPrice + ' zł');
+    cy.get('.product')
+      .eq(4)
+      .find('.product-image')
+      .should(
+        'have.css',
+        'background-image',
+        'url("http://localhost:4200/assets/hotdog.jpg")'
+      );
+  });
 
-  it('should edit a product in the store', () => {});
+  it('should remove a product from the store', () => {
+    cy.get('.product')
+      .eq(0)
+      .trigger('mouseover', { force: true })
+      .find('[data-cy="product-delete-button"]')
+      .click({ force: true });
+
+    cy.get('.product').should('have.length', 3);
+  });
+
+  it('should edit a product in the store', () => {
+    const editedName = 'Edited';
+    cy.get('.product')
+      .eq(0)
+      .find('[data-cy="product-edit-button"]')
+      .click({ force: true });
+    cy.get('[data-cy="product-name-input"]').type(editedName);
+    cy.get('[data-cy="add-edit-save-button"]').click();
+
+    cy.get('.product')
+      .eq(0)
+      .find('.product-name')
+      .should('contain.text', 'Burger' + editedName);
+  });
 });
