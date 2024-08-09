9a59bab70fa121f7a65e25c1491c3833027a10a6
sebmat003 <sebmat003@utp.edu.pl>
2024-08-09 12:08:22 +0200

    removed moduleDirectories

--- angular-testing-completed/jest.config.js
+++ angular-testing-completed/jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: "jest-preset-angular",
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-  moduleDirectories: ["node_modules", "<rootDir>"],
   transformIgnorePatterns: [
     "<rootDir>/node_modules/(?!lodash-es|@angular|@ngneat|ng2-charts|@ngrx)",
   ],
